2f775b3a1931cbb09ab47e98ed6bfbae85da170c
Lazuardi Mahendra <lazuardhie.mahendra@gmail.com>
2024-06-19 19:57:47 +0700

    Create JavaLoopsII.java

---
+++ Java/JavaLoopsII.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+             
+            double hasil = 0;
+            for(int j =0; j < n ; j++){
+                pangkat = 
+                System.out.print(pangkat + " ");
+                hasil = pangkat * b;
+                a += hasil;
+                // System.out.print(a + " ");
+            }
+            // System.out.println();
+        }
+        in.close();
+    }
+    
+   
+}
