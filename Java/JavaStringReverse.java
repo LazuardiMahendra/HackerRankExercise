5be664478ff5e7ed802dac4f4c5dbb1eb4923f23
Lazuardi Mahendra <lazuardhie.mahendra@gmail.com>
2024-06-26 11:24:55 +0700

    Create JavaStringReverse.java

---
+++ Java/JavaStringReverse.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        
+        String temp = "";
+        
+        for(int i = A.length() -1; i >= 0; i --){
+            temp += A.charAt(i);
+        }
+        
+        if(A.equals(temp)){
+            System.out.println("Yes");
+        } else{
+            System.out.println("No");
+        }
+    }
+}
+
+
+
